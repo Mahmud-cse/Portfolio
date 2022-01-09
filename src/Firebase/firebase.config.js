024d78d31d3204661615f6235fd1fc3633d18588
Mahmud-cse <mahmudhasan555666@gmail.com>
2022-01-09 17:53:35 +0600

    done

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyB_oFFc4mXNIe0a9Hz6fGOliVlZGGhEndo',
+  authDomain: 'mahmud-portfolio-9.firebaseapp.com',
+  projectId: 'mahmud-portfolio-9',
+  storageBucket: 'mahmud-portfolio-9.appspot.com',
+  messagingSenderId: '10740344450',
+  appId: '1:10740344450:web:d3d54f9562f08d6eafb69b',
+};
+
+export default firebaseConfig;
